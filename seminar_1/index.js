b7be3c6e9132b8fa0e43a2d6c8b678820f6164de
Alexandra <avisinescu59@yahoo.com>
2025-10-07 19:18:06 +0300

    pas 4 commit

--- seminar_1/index.js
+++ seminar_1/index.js
@@ -1,9 +1,13 @@
-var http = require("http")
+const express =require('express')
 
-http
-    .createServer(function (req, res){
-        res.write("Hello Web!")
-        res.end()
-    })
-    .listen(8080)
-    
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello world!')
+})
+
+app.get('/ping', (req, res)=>{
+    res.send('pong')
+})
+
+app.listen(8080)
